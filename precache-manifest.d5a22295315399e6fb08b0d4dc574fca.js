b847861d28273c1205b7c5bd9c2e3245fa021acc
arash-fun <arash.funlife@gmail.com>
2022-09-19 11:08:40 +0430

    Updates

---
+++ precache-manifest.d5a22295315399e6fb08b0d4dc574fca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45e0a602235ff935eac8548a6637aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd1c606a638dca7d8473",
+    "url": "/static/js/2.ae360d71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ae360d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1619fa7131e99193f28",
+    "url": "/static/js/main.877b14b6.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
